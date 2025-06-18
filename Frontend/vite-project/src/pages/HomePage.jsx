002e5dbcc1f5957c437dfc04143429b63f6cd64e
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-06-18 23:33:07 +0545

    Comp: Rate limit component / ui

--- Frontend/vite-project/src/pages/HomePage.jsx
+++ Frontend/vite-project/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import { toast } from "react-hot-toast";
+import Navbar from "../components/Navbar";
+import RateLimitUI from "../components/RateLimitUI";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRatelimited] = useState(false);
+
   return (
-    <div className="p-4">
+    <div className="p-4 min-h-screen bg-base-100">
+      <Navbar />
+      {isRateLimited && <RateLimitUI />}
       <h1 className="text-2xl font-bold mb-4">Home Page</h1>
       <button onClick={() => toast.success("First Toast Test!")}>
         Show Toast
