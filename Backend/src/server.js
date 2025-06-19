f3487d32b865dae3d304758c82b17dd612ca01b6
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-06-19 23:14:27 +0545

    Feat: backend data display in frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,6 +6,7 @@ import noteRoutes from "./routes/noteRoutes.js";
 import { connectdb } from "./config/db.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middlware/ratelimiter.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -14,12 +15,19 @@ const PORT = process.env.PORT || 5001;
 // connectdb();
 
 //Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    // credentials: true,
+  })
+);
 app.use(express.json());
 app.use(ratelimiter); //::1 localhost for ip
+
 app.use("/api/notes/", noteRoutes);
 
 connectdb().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
   });
-}); 
+});
--- Frontend/vite-project/src/pages/HomePage.jsx
+++ Frontend/vite-project/src/pages/HomePage.jsx
@@ -37,6 +37,24 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading ? (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        ) : isRateLimited ? null : notes.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {notes.map((note, idx) => (
+              <div key={idx}>
+                {note.title} || {note.content}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center text-gray-500 py-10">
+            No notes available.
+          </div>
+        )}
+      </div>
     </div>
   );
 };
