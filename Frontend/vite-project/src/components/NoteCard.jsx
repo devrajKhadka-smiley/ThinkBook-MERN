66a1c0c5c67e9b550bac54469bd07d82a051fb8e
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-06-20 09:45:41 +0545

    feat: Delete and improved error handling

--- Frontend/vite-project/src/components/NoteCard.jsx
+++ Frontend/vite-project/src/components/NoteCard.jsx
@@ -2,8 +2,31 @@ import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
 import React from "react";
 import { formatDate } from "../lib/utils";
+import axiosInstance from "../lib/axios";
+import toast from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await axiosInstance.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id));
+      toast.success("Note deleted successfully!");
+    } catch (error) {
+      if (error.response.status === 429) {
+        toast.error("Slow Down man! you'r going crazy!", {
+          duration: 4000,
+          icon: "🩻",
+        });
+      } else {
+        toast.error("Failed to delete note. Please try again later.");
+        console.error("Error deleting note:", error);
+      }
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -18,7 +41,10 @@ const NoteCard = ({ note }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
--- Frontend/vite-project/src/pages/HomePage.jsx
+++ Frontend/vite-project/src/pages/HomePage.jsx
@@ -45,7 +45,7 @@ const HomePage = () => {
         ) : isRateLimited ? null : notes.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
             {notes.map((note, idx) => (
-             <NoteCard  key={note._id} note={note}/>
+             <NoteCard  key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         ) : (
