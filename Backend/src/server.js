72caadb01465123c90bdded6929a91cb538d47b2
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-06-21 01:38:16 +0545

    feat: prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,25 +7,39 @@ import { connectdb } from "./config/db.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middlware/ratelimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 // connectdb();
 
 //Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    // credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      // credentials: true,
+    })
+  );
+}
+
 app.use(express.json());
 app.use(ratelimiter); //::1 localhost for ip
 
 app.use("/api/notes/", noteRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "../Frontend/vite-project/dist"))
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../Frontend/vite-project/dist/index.html")
+    );
+  });
+}
 connectdb().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
--- Frontend/vite-project/src/lib/axios.js
+++ Frontend/vite-project/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/axiosInstance";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:5001/api",
